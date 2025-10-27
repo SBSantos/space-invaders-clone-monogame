602ffd31cece8d4bcb152eb177999236722d9808
Funkenstein <Saymon.801.sb@gmail.com>
2025-10-27 02:20:10 -0300

    feat: add tilemaps and collisions

--- SpaceInvadersClone/GameLoop.cs
+++ SpaceInvadersClone/GameLoop.cs
@@ -5,15 +5,22 @@ using Microsoft.Xna.Framework.Input;
 using GameLibrary;
 using GameLibrary.Graphics;
 using GameLibrary.Input;
-using System.Globalization;
 
 namespace SpaceInvadersClone;
 
 public class GameLoop : Core
 {
     private AnimatedSprite _player;
+    private AnimatedSprite _croach;
 
     private Vector2 _playerPosition;
+    private Vector2 _resetPlayerPosition;
+
+    private Vector2 _croachPosition;
+
+    private Tilemap _tilemap;
+
+    private Rectangle _roomBounds;
 
     private const float MOVEMENT_SPEED = 5.0f;
 
@@ -29,6 +36,30 @@ public class GameLoop : Core
         // TODO: Add your initialization logic here
 
         base.Initialize();
+
+        Rectangle screenBounds = GraphicsDevice.PresentationParameters.Bounds;
+
+        _roomBounds = new(
+            (int)_tilemap.TileWidth,
+            (int)_tilemap.TileHeight,
+            screenBounds.Width - (int)_tilemap.TileWidth * 2,
+            screenBounds.Height - (int)_tilemap.TileHeight * 2
+        );
+
+        const float DESIRED_ROW = 1.2f;
+        int row = (int)(_tilemap.Rows / DESIRED_ROW);
+        int playerColumn = _tilemap.Columns / 2;
+        _playerPosition = new(
+            x: playerColumn * _tilemap.TileWidth,
+            y: row * _tilemap.TileHeight
+        );
+        _resetPlayerPosition = _playerPosition;
+
+        int croachColumn = (int)(_tilemap.Columns / DESIRED_ROW);
+        _croachPosition = new(
+            x: croachColumn * _tilemap.TileWidth,
+            y: row * _tilemap.TileHeight
+        );
     }
 
     protected override void LoadContent()
@@ -39,11 +70,19 @@ public class GameLoop : Core
             content: Content,
             filename: "images/atlas-definition.xml"
         );
-        const float SCALE = 10.0f;
+        const float SCALE = 2.0f;
 
         _player = atlas.CreateAnimatedSprite(animationName: "player-animation");
         _player.Scale = new Vector2(x: SCALE, y: SCALE);
-        _player.CenterOrigin();
+
+        _croach = atlas.CreateAnimatedSprite(animationName: "croach-animation");
+        _croach.Scale = new Vector2(x: SCALE, y: SCALE);
+
+        _tilemap = Tilemap.FromFile(
+            content: Content,
+            filename: "images/tilemap-definition.xml"
+        );
+        _tilemap.Scale = new(x: SCALE, y: SCALE);
 
         base.LoadContent();
     }
@@ -52,8 +91,37 @@ public class GameLoop : Core
     {
         // TODO: Add your update logic here
         _player.Update(gameTime);
+        _croach.Update(gameTime);
 
         CheckKeyboardInput();
+
+        Rectangle playerBounds = new(
+            x: (int)_playerPosition.X,
+            y: (int)_playerPosition.Y,
+            width: (int)_player.Width,
+            height: (int)_player.Height
+        );
+
+        Rectangle croachBounds = new(
+            x: (int)_croachPosition.X,
+            y: (int)_croachPosition.Y,
+            width: (int)_croach.Width,
+            height: (int)_croach.Height
+        );
+
+        if (playerBounds.Left < _roomBounds.Left)
+        {
+            _playerPosition.X = _roomBounds.Left;
+        }
+        else if (playerBounds.Right > _roomBounds.Right)
+        {
+            _playerPosition.X = _roomBounds.Right - _player.Width;
+        }
+
+        if (playerBounds.Intersects(croachBounds))
+        {
+            _playerPosition = _resetPlayerPosition;
+        }
 
         base.Update(gameTime);
     }
@@ -90,22 +158,21 @@ public class GameLoop : Core
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
-        const float HALF = 0.5f;
-
-        // Middle of the screen on the x-axis
-        float MiddleX = GraphicsDevice.PresentationParameters.BackBufferWidth * HALF;
-
-        // Middle of the screen on the y-axis
-        float MiddleY = GraphicsDevice.PresentationParameters.BackBufferHeight * HALF;
 
         SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        _tilemap.Draw(SpriteBatch);
+
         _player.Draw(
             spriteBatch: SpriteBatch,
-            position: new Vector2(
-                _playerPosition.X + MiddleX,
-                _playerPosition.Y + MiddleY
-            )
+            position: _playerPosition
         );
+
+        _croach.Draw(
+            spriteBatch: SpriteBatch,
+            position: _croachPosition
+        );
+
         SpriteBatch.End();
 
         base.Draw(gameTime);
